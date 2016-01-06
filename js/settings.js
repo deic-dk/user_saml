2f17d1e217dfae48e69e0259514c9afe23fb453c
deltafunction <frederik@orellana.dk>
2016-01-06 11:26:48 +0100

    Layout.

--- js/settings.js
+++ js/settings.js
@@ -1,3 +1,10 @@
 $(document).ready(function() {
 	$('#samlSettings').tabs();
+	$('#samlSettings').removeClass('ui-widget');
+	$('#samlSettings').removeClass('ui-widget-content');
+	$('#samlSettings').removeClass('ui-corner-all');
+	$('#samlSettings ul').removeClass('ui-widget');
+	$('#samlSettings ul').removeClass('ui-widget-header');
+	$('#samlSettings ul').removeClass('ui-corner-all');
+	$('#samlSettings').css('padding', '2.0em');
 });
