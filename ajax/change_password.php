ec604d4b98353e9e1bfb738a811743e853faa3e1
deltafunction <frederik@orellana.dk>
2016-06-16 14:43:17 +0200

    Password fixes.

--- ajax/change_password.php
+++ ajax/change_password.php
@@ -1,17 +1,12 @@
 <?php
 
-
-
-
-OCP\JSON::checkAppEnabled('importer');
+OCP\JSON::checkAppEnabled('user_saml');
+OCP\JSON::checkAppEnabled('files_sharding');
 OC_JSON::checkLoggedIn();
-
-
 
 $username = OC_User::getUser();
 $password = isset($_POST['personal-password']) ? $_POST['personal-password'] : null;
 $oldPassword = isset($_POST['oldpassword']) ? $_POST['oldpassword'] : '';
-
 
 $cracklibCheck = shell_exec("echo $password | /usr/local/sbin/cracklib-check 2>&1 | xargs echo -n");
 
@@ -21,7 +16,6 @@ if(substr($cracklibCheck, -4)!=": OK"){
 	\OC_JSON::error(array("data" => array("message" => $cracklibCheck)));
 	exit();
 }
-
 
 /*if (!\OC_User::checkPassword($username, $oldPassword)) {
 	$l = new \OC_L10n('settings');
@@ -34,7 +28,7 @@ if (!is_null($password) && \OC_User::setPassword($username, $password)) {
 		if(\OCA\FilesSharding\Lib::isMaster() && !\OCA\FilesSharding\Lib::onServerForUser($username)){
 			// Update on the home server (to allow local/independent login)
 			$serverURL = \OCA\FilesSharding\Lib::getServerForUser($username, true);
-			$pwOk = ws('set_pw_hash', array('user_id'=>$user_id), true, true, $serverURL);
+			$pwOk = \OCA\FilesSharding\Lib::ws('set_pw_hash', array('user_id'=>$username), true, true, $serverURL);
 			if($pwOk['status']!='success'){
 				OC_Log::write('ChangePassword','ERROR: Could not change password for: '.
 				$username." on ".$serverURL, \OC_Log::ERROR);
